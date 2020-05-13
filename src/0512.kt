f2065c31b1112ed4ea00f40c8c6a53b834330584
ming <1398565099@qq.com>
2020-05-13 22:12:10 +0800

    0512新增部分空安全，可控类型操作
    Signed-off-by: ming <1398565099@qq.com>

--- src/0512.kt
+++ src/0512.kt
@@ -1,4 +1,3 @@
-
 fun main() {
     val computer = Number(11, 20) { x, y -> x + y }
     computer.operation()
@@ -13,6 +12,27 @@ fun main() {
     println(age.isAudlt)
     age.addAge = 10
     println(age.isAudlt)
+
+    val builder = Test.Builder().setName("张三")?.setAge(26)?.setSex("男")
+    println(builder)
+
+    let()
+}
+
+fun let() {
+    //let操作符去掉null元素
+    val listOf = listOf(1, 3, null, 8, "what", 9, 4, null,"9077")
+    for (index in listOf) {
+        if (index != null) {
+            println("index-->$index")
+        }
+    }
+
+    for (index in listOf) {
+        index?.let {
+            println("index2-->$index")
+        }
+    }
 }
 
 
@@ -39,4 +59,31 @@ class Age(age: Int) {
 //                isAudlt = true
 //            }
         }
+}
+
+class Test {
+    class Builder {
+        var name: String? = "Tom"
+        var age: Int? = 0
+        var sex: String? = "man"
+
+        fun setName(name: String): Builder? {
+            this.name = name
+            return this
+        }
+
+        fun setAge(age: Int): Builder? {
+            this.age = age
+            return this
+        }
+
+        fun setSex(sex: String): Builder? {
+            this.sex = sex
+            return this
+        }
+
+        override fun toString(): String {
+            return "Builder(name:$name,age:$age,sex:$sex)"
+        }
+    }
 }
